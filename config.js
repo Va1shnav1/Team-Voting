536b05535e1986464be5584f0590ddda5f6ecd92
Va1shnav1 <vatsava1012@gmail.com>
2021-11-28 20:18:51 -0500

    updated database

--- config.js
+++ config.js
@@ -2,7 +2,12 @@ import firebase from 'firebase';
 
 // add SDK Firebase
 var firebaseConfig = {
-
+    apiKey: "AIzaSyAHfifA-CdNyPo9txa5yRS_64CcM1vLwMs",
+  authDomain: "project-67-7edce.firebaseapp.com",
+  projectId: "project-67-7edce",
+  storageBucket: "project-67-7edce.appspot.com",
+  messagingSenderId: "838133154040",
+  appId: "1:838133154040:web:71836cc1aed0deb0e03b0d"
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
